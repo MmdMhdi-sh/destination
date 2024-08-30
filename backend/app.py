47678ab97de7204977d37cca9f8bc647bf916daa
mmdmhdi <mmdmhdi.shhr@gmail.com>
2024-08-30 12:57:05 +0330

    CRUD views and templates

--- backend/app.py
+++ backend/app.py
@@ -10,8 +10,8 @@ class City(db.Model):
     _id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(150), unique=True, nullable=False)
 
-    def __repr__(self):
-        return '<User %r>' % self.name
+    # def __repr__(self):
+    #     return '<User %r>' % self.name
 with app.app_context():
     db.create_all()
 
@@ -21,6 +21,7 @@ def home_page():
     if request.method == "POST":
         city_name = request.form['name']
         new_city = City(name=city_name)
+        print("new city = ", new_city)
 
         try:
             db.session.add(new_city)
@@ -31,6 +32,34 @@ def home_page():
     else:
         cities_list = City.query.order_by(City._id).all()
         return render_template('pages/home.html', cities=cities_list)
+
+@app.route('/delete/<int:id>')
+def city_delete_view(id):
+    city_obj = City.query.get_or_404(id)
+    try:
+        db.session.delete(city_obj)
+        db.session.commit()
+        return redirect('/')
+    except:
+        return "You cannot delete this city!"
+
+@app.route('/detail/<int:id>', methods=['GET'])
+def city_detail_view(id):
+    city_obj = City.query.get_or_404(id)
+    return render_template('pages/city.html', city=city_obj)
+
+@app.route('/update/<int:id>', methods=['GET', 'POST'])
+def city_update_view(id):
+    city_obj = City.query.get_or_404(id)
+    if request.method == "POST":
+        city_obj.name = request.form['name']
+        try:
+            db.session.commit()
+            return redirect('/')
+        except:
+            return 'City cannot be updated'
+    return render_template('pages/city_update.html', city=city_obj)
+    
 
 if __name__ == '__main__':
     app.run(debug=True)
