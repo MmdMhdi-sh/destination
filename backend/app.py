5cbc939fc608a03ae6fd939994d8206930eb21cb
mmdmhdi <mmdmhdi.shhr@gmail.com>
2024-08-31 22:07:56 +0330

    comments

--- backend/app.py
+++ backend/app.py
@@ -3,10 +3,11 @@ from flask import (
     redirect,
     render_template,
     request, 
-    session
+    session,
+    url_for
 )
 from flask_sqlalchemy import SQLAlchemy
-from datetime import timedelta
+from datetime import timedelta, datetime
 
 app = Flask(__name__)
 
@@ -29,10 +30,27 @@ class User(db.Model):
 class City(db.Model):
     _id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(150), unique=True, nullable=False)
-
+    posts = db.relationship('Post', backref='city', lazy=True)
     def __repr__(self):
         return '<City %r>' % self.name
     
+class Post(db.Model):
+    _id = db.Column(db.Integer, primary_key=True)
+    city_id = db.Column(db.String(150), db.ForeignKey('city._id'), nullable=False)
+    content = db.Column(db.String(500), nullable=False)
+
+    def __repr__(self):
+        return '<Post %r>' % self._id
+
+class Comment(db.Model):
+    _id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.String(150), db.ForeignKey('user._id'), nullable=False)
+    post_id = db.Column(db.String(150), db.ForeignKey('post._id'), nullable=False)
+    content = db.Column(db.String(500), nullable=False)
+    timestamp = db.Column(db.DateTime,
+        nullable=False, unique=False, index=False,
+        default=datetime.utcnow)
+
 with app.app_context():
     db.create_all()
 
@@ -116,6 +134,43 @@ def login_view():
     return render_template("pages/login.html")
     
 
+@app.route('/posts/<city_name>', methods=['GET', 'POST'])
+def city_posts_view(city_name):
+    if request.method == 'POST':
+        # post_user = User.query.filter_by(username=request.form['username'])
+        post_city = City.query.filter_by(name=city_name).first()
+        post_city_id = post_city._id
+        post_content = request.form['content']
+        new_post = Post(city_id=post_city_id, content=post_content)
+        try:
+            db.session.add(new_post)
+            db.session.commit()
+            return redirect(url_for('city_posts_view', city_name=post_city.name))
+        except:
+            return 'You cannot add this post!'
+    print("city_name = ",city_name)
+    city_obj= City.query.filter_by(name=city_name).first()
+    posts_list = City.query.filter_by(name=city_name).first().posts
+    return render_template('city/city_posts.html', city=city_obj, posts=posts_list)
+
+@app.route('/posts/<int:id>', methods=['POST', 'GET'])
+def post_detail_view(id):
+    post_obj = Post.query.get_or_404(id)
+    if request.method == 'POST':
+        comment_user = User.query.get_or_404(1)
+        comment_post = Post.query.get_or_404(id)
+        comment_content = request.form['content']
+        new_comment = Comment(user_id=comment_user._id, post_id=comment_post._id, content=comment_content)
+        try:
+            db.session.add(new_comment)
+            db.session.commit()
+            comments_list = Comment.query.filter_by(post_id=id).all()
+            return redirect(url_for('post_detail_view', id=comment_post._id))
+        except:
+            return 'You cannot add this comment!'
+    comments_list = Comment.query.filter_by(post_id=id).all()
+    return render_template('post/detail.html', post=post_obj, comments=comments_list)
+
 
 
 @app.route('/delete/<int:id>')
@@ -131,7 +186,7 @@ def city_delete_view(id):
 @app.route('/detail/<int:id>', methods=['GET'])
 def city_detail_view(id):
     city_obj = City.query.get_or_404(id)
-    return render_template('pages/city.html', city=city_obj)
+    return render_template('city/city_detail.html', city=city_obj)
 
 @app.route('/update/<int:id>', methods=['GET', 'POST'])
 def city_update_view(id):
@@ -144,7 +199,17 @@ def city_update_view(id):
         except:
             return 'City cannot be updated'
     return render_template('pages/city_update.html', city=city_obj)
-    
+
+
+"""
+API Views
+"""
+@app.route('/api/post/<int:id>/comments')
+def city_comments_api_view(id):
+    comments = Comment.query.filter_by(post_id=id).all()
+    username = User.query.get_or_404(1).username
+    serializer = [{"username": username, "content": comment.content} for comment in comments]
+    return render_template('api/comment.html', comments=serializer)
 
 if __name__ == '__main__':
     app.run(debug=True)
